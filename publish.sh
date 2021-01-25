961b8d96fb9ef83c11099370d9467d24564d1897
GuoJinMing@mbp <tdouguo@163.com>
2021-01-25 17:22:42 +0800

    up shell

--- publish.sh
+++ publish.sh
@@ -1,19 +1,26 @@
-cat Assets/ReferencePool/package.json | jq 'del(.version)' | jq '. + { "version": "sdkver" }'>>tmp
-mv tmp Assets/ReferencePool/package.json
-sed -i '' 's/sdkver/'"$1"'/'  Assets/ReferencePool/package.json
-git add -A
-git commit -m "update version"
-git pull
-git push
+#!/bin/bash
 
-git subtree split --prefix=Assets/ReferencePool --branch upm
-git tag $1 upm
-git push origin upm --tags
+case $1 in
+    deltag)
+        echo 'delete tag '${2}
+        # delete tab
+		git tag -d $2
+		git push origin :refs/tags/$2
+        ;;
+    help)
+        echo 'input arg ( deltag | {version} build upm and tag )'
+        ;;
+    *)
+		cat Assets/ReferencePool/package.json | jq 'del(.version)' | jq '. + { "version": "sdkver" }'>>tmp
+		mv tmp Assets/ReferencePool/package.json
+		sed -i '' 's/sdkver/'"$1"'/'  Assets/ReferencePool/package.json
+		git add -A
+		git commit -m "update version"
+		git pull
+		git push
 
-# set upm version
-# git subtree split --prefix=Assets/ReferencePool --branch upm$1
-# git tag $1 upm$1
-
-# delete tab
-# git tag -d $1
-# git push origin :refs/tags/$1
+		git subtree split --prefix=Assets/ReferencePool --branch upm
+		git tag $1 upm
+		git push origin upm --tags
+        ;;
+esac
