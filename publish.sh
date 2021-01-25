21f8d323e085d25318ad489608a9341b122877e6
GuoJinMing@mbp <tdouguo@163.com>
2021-01-25 16:45:48 +0800

    up shell

--- publish.sh
+++ publish.sh
@@ -5,8 +5,15 @@ git add -A
 git commit -m "update version"
 git pull
 git push
-# git subtree split --prefix=Assets/ReferencePool --branch upm$1
-# git tag $1 upm$1
+
 git subtree split --prefix=Assets/ReferencePool --branch upm
 git tag $1 upm
 git push origin upm --tags
+
+# set upm version
+# git subtree split --prefix=Assets/ReferencePool --branch upm$1
+# git tag $1 upm$1
+
+# delete tab
+# git tag -d $1
+# git push origin :refs/tags/$1
